6e183f39f091e81876601ea29125d924deb946a3
Nathan Dullea <njdullea@gmail.com>
2017-10-12 21:24:22 -0500

    with anim

---
+++ Assets/Scripts/explosionController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class explosionController : MonoBehaviour {
+
+	public Sprite[] frames;
+
+	private SpriteRenderer rend;
+	private float timePerFrame = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<SpriteRenderer> ();
+		rend.sprite = frames [0];
+
+		StartCoroutine (Explode ());
+	}
+
+	IEnumerator Explode() {
+		yield return new WaitForSeconds (timePerFrame);
+		for (int i = 1; i < frames.Length; i++) {
+			rend.sprite = frames [i];
+			yield return new WaitForSeconds (timePerFrame);
+		}
+		//Destroy (gameObject);
+		SceneManager.LoadScene("endScene");
+	}
+
+}
---
+++ Assets/Scripts/shootBomb.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootBomb : MonoBehaviour {
+
+	public int toolCount;
+	public int bombs;
+	public int toolsToBombs;
+	private int direction; 
+	public GameObject bomb;
+
+	// Use this for initialization
+	void Start () {
+		toolCount = 0;
+		bombs = 2;
+		toolsToBombs = 2;
+		direction = PlayerControl.playercontrol.direction;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (toolCount >= toolsToBombs) {
+			print ("adding a bomb");
+			bombs += 1;
+			toolCount -= toolsToBombs;
+		} else {
+			//print ("not adding a bomb");
+		}
+
+		direction = PlayerControl.playercontrol.direction;
+
+		if (Input.GetKeyDown(KeyCode.D)) {
+			fireBomb();
+		}
+	}
+
+	void fireBomb () {
+		print ("firing");
+		if (bombs > 0) {
+			GameObject obj = (GameObject)Instantiate (bomb, transform.position, transform.rotation);
+			bombs = bombs - 1;
+			print ("Shot a bomb");
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D thing) {
+		if (thing.gameObject.tag == "tools") {
+			print ("picked up tools");
+			toolCount += 1;
+		}
+	}
+}
